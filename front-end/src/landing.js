5fd6c6a1b3c29714dfa5fc71bd1c11092624ddec
Rahisul Haque <rahisul@icloud.com>
2018-10-21 05:22:31 -0700

    something done

---
+++ front-end/src/landing.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+ 
+class Landing extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Sign-In</h2>
+        
+      </div>
+    );
+  }
+}
+ 
+export default Landing;
---
+++ front-end/src/progress.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Progress extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+}
+ 
+export default Progress;
