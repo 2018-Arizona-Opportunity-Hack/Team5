eab794a003c05d5a8eb480552a6d915b823f05f3
Rahisul Haque <rahisul@icloud.com>
2018-10-21 13:10:44 -0700

    coreection in a graph. Rahi

--- front-end/src/chart.js
+++ front-end/src/chart.js
@@ -39,7 +39,7 @@ class Chart extends Component{
           options={{
             title:{
               display:this.props.displayTitle,
-              text:'Potential customer by 2020:'+this.props.location,
+              text:'Pets without home:'+this.props.location,
               fontSize:25
             },
             legend:{
